60af62cb395a4f84f791d0417db844727ea5abc9
Talib Idris <talib.idris@annodata.co.uk>
2018-07-20 12:51:24 +0100

    Add egress rule

--- jenkins.tf
+++ jenkins.tf
@@ -39,4 +39,11 @@ resource "aws_security_group" "jenkins-security-group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
